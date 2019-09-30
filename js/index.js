f7194c867734c111b1c14dd89bc77b302e0b2a34
emmacodes19 <berdae@gmail.com>
2019-09-30 15:34:49 -0500

    mvp

--- js/index.js
+++ js/index.js
@@ -40,3 +40,101 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+//All Images
+
+const circle = document.getElementById('cta-img');
+circle.setAttribute('src', siteContent['cta'] ['img-src'])
+
+const code_snippets = document.getElementById('middle-img');
+code_snippets.setAttribute('src', siteContent['main-content'] ['middle-img-src']);
+
+// Title 
+
+const title = document.querySelector('h1');
+title.textContent = 'DOM is Awesome';
+
+//Button
+
+const button = document.querySelector('button');
+button.textContent = 'Get Started';
+
+// Nav
+
+const navigation = document.querySelectorAll('a');
+navigation[0].textContent = siteContent ['nav']['nav-item-1'];
+navigation[1].textContent = siteContent ['nav']['nav-item-2'];
+navigation[2].textContent = siteContent ['nav']['nav-item-3'];
+navigation[3].textContent = siteContent ['nav']['nav-item-4'];
+navigation[4].textContent = siteContent ['nav']['nav-item-5'];
+navigation[5].textContent = siteContent ['nav']['nav-item-6'];
+
+//Change nav color to green 
+
+Array.from(navigation)
+Array.from(navigation).forEach((link)=> {
+  link.style.color ='green';
+})
+
+// or do this, it works too 
+
+// navigation[0].style.color ='green';
+
+
+
+
+
+//h4 subtitles
+
+const subtitles = document.querySelectorAll('.text-content h4');
+subtitles[0].textContent = "Features";
+subtitles[1].textContent = "About";
+subtitles[2].textContent = "Services";
+subtitles[3].textContent = "Product";
+subtitles[4].textContent = "Vision";
+
+
+// all p 
+
+const paragraphs = document.querySelectorAll('.text-content p');
+paragraphs[0].textContent = siteContent['main-content'] ['features-content'];
+paragraphs[1].textContent = siteContent['main-content'] ['about-content'];
+paragraphs[2].textContent = siteContent['main-content'] ['services-content'];
+paragraphs[3].textContent = siteContent['main-content'] ['product-content'];
+paragraphs[4].textContent = siteContent['main-content'] ['vision-content'];
+
+
+//contact and Info 
+
+const contact = document.querySelector('.contact h4');
+contact.innerHTML = siteContent['contact'] ['contact-h4'];
+
+const info = document.querySelectorAll('.contact p');
+info[0].innerHTML = siteContent['contact'] ['address'];
+info[1].innerHTML = siteContent['contact'] ['phone'];
+info[2].innerHTML = siteContent['contact'] ['email'];
+
+
+
+
+//copyright 
+const copy = document.querySelector('footer p');
+copy.textContent = 'Copyright Great Idea! 2018';
+
+
+// Item Creation Step 1: create a new element
+const newText = document.createElement("a");
+// Item Creation Step 2: add some content to the newly created div
+newText.textContent = "prepend";
+// Item Creation Step 3: prepend or append the element to an existing DOM node on the page
+const parentContent = document.querySelector('nav');
+parentContent.prepend(newText);
+
+
+const newAnchor = document.createElement('a');
+newAnchor.textContent = "apprendChild";
+
+const newParent = document.querySelector('nav');
+newParent.appendChild(newAnchor);
+
+
